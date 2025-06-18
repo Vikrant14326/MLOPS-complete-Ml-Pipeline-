3df45497caed43539606255f19eea8f4edef71fd
vikrant <virat845712@gmail.com>
2025-06-18 10:47:33 +0530

    params configured for the pipeline

--- src/model_building.py
+++ src/model_building.py
@@ -119,11 +119,11 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        # params = load_params('params.yaml')['model_building']
-        params= {
-            'n_estimators': 26,
-            'random_state': 4
-        }
+        params = load_params('params.yaml')['model_building']
+        # params= {
+        #     'n_estimators': 26,
+        #     'random_state': 4
+        # }
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
